9097db8daa5d2db4ba95dea00a52e1d716b1505c
knyazev-ro <knyazevseven@gmail.com>
2026-02-08 22:57:09 +0300

    chunked and map and each

--- orm/vulcan/chunked.go
+++ orm/vulcan/chunked.go
@@ -3,10 +3,22 @@ package vulcan
 import "context"
 
 func (q *Query[T]) Chunk(ctx context.Context, chunk int, closure func([]T) error) error {
-	arr := []T{}
-	err := closure(arr)
-	if err != nil {
-		return err
+	for {
+		data, err := q.CursorPaginate("id", nil, chunk).Load(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		if len(data) <= 0 {
+			break
+		}
+
+		err = closure(data)
+
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -25,4 +37,22 @@ func (q *Query[T]) Each(ctx context.Context, closure func(T) error) error {
 		return err
 	}
 	return nil
+}
+
+func (q *Query[T]) Map(ctx context.Context, closure func(T) (T, error)) ([]T, error) {
+	result := []T{}
+	err := q.Chunk(ctx, 1000, func(t []T) error {
+		for _, elem := range t {
+			model, err := closure(elem)
+			if err != nil {
+				return err
+			}
+			result = append(result, model)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
